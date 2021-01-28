4f2460ec09fe9c61065beeac9b3e5a17f28f2573
Dschimm <rhotert@stud.uni-hannover.de>
2021-01-28 16:58:06 +0100

    change path

--- src/play.py
+++ src/play.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("python /home/jim/Code/uni/.venv/lib/python3.8/site-packages/gym/envs/box2d/car_racing.py")
+os.system("python ../.venv/lib/python3.8/site-packages/gym/envs/box2d/car_racing.py")
