dabc1c5e96475254e56d569bbf58812205d2cc72
Jim <rhotert@stud.uni-hannover.de>
2021-02-04 17:24:57 +0100

    epsGreedyPolicy

--- src/policy.py
+++ src/policy.py
@@ -1,18 +1,27 @@
 import numpy as np
-
+import torch
+ 
 class RandomPolicy():
     
     def __init__(self, seed=None):
         np.random.seed(seed)
 
     def action(self, state):
-        return 0
-        #return np.random.randint(9)
+        return np.random.randint(9)
 
 class eGreedyPolicy():
 
-    def __init__(self, env, seed, eps):
-        pass
+    def __init__(self, env, seed, eps, model):
+        self.eps = eps 
+        np.random.seed(seed)
+        self.env = env
+        self.model = model
+           
 
     def action(self, state):
-        pass
+        if np.random.uniform(0, 1) < self.eps:
+            return self.env.action_space.sample()
+        state = torch.tensor(state).unsqueeze(0).float()
+        q_values = self.model(state)
+        return torch.max(q_values, dim=1)[1].item()
+
--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ import gym
 from gym.wrappers.gray_scale_observation import GrayScaleObservation
 from collections import deque
 
-from policy import RandomPolicy
+from policy import RandomPolicy, eGreedyPolicy
 from dqn import DQN
 
 
@@ -19,7 +19,7 @@ from gym_utils import ActionWrapper
 
 
 
-def train(env, model, policy, EPISODES=1000, EPISODE_LENGTH = 100, BATCH_SIZE = 64):
+def train(env, model, policy, EPISODES=1000, EPISODE_LENGTH = 200, BATCH_SIZE = 64):
     
     rewards = []
     replay_buffer = deque(maxlen=int(1e5))
@@ -27,7 +27,9 @@ def train(env, model, policy, EPISODES=1000, EPISODE_LENGTH = 100, BATCH_SIZE = 
     for i in range(EPISODES):
         state = env.reset()
         print("Episode", i)
-        for _ in range(EPISODE_LENGTH):    
+        for _ in range(EPISODE_LENGTH):  
+            #env.render()
+            print(".", end="", flush=True)
             action = policy.action(state)
             next_state, reward, done, meta = env.step(action)
 
@@ -52,8 +54,9 @@ def train(env, model, policy, EPISODES=1000, EPISODE_LENGTH = 100, BATCH_SIZE = 
     return
 
 if __name__ == "__main__":
+    seed = 42
     env = ActionWrapper(GrayScaleObservation(gym.make('CarRacing-v0')))
-    env.seed(42)
+    env.seed(seed)
     dqn = DQN(env)
-    policy = RandomPolicy(42)
+    policy = eGreedyPolicy(env, seed, 0.1, dqn)
     train(env, dqn, policy)
